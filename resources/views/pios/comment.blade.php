39b81cfee139051a2353b593a77f08ffa46c6a69
SirunoDris <j.sirunodri.elluch@gmail.com>
2023-03-20 01:05:59 +0100

    View changes.

--- resources/views/pios/comment.blade.php
+++ resources/views/pios/comment.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-        <h1>COMMETN VIEW</h1>
+        <h1>COMMENT PAGE</h1>
         <div class="mt-4 bg-white shadow-sm rounded-lg divide-y">
             <div class="p-6 flex space-x2">
                 <div class="flex-1">
@@ -54,7 +54,7 @@
         {{old('message')}}
         </textarea>
         <x-input-error :messages="$errors->get('message')" class="mt-2"/>
-        <x-primary-button class="mt-4">{{__('Pio')}}</x-primary-button> 
+        <x-primary-button class="mt-4">{{__('Comentar')}}</x-primary-button> 
         
     </form>
 
