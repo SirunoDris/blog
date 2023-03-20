087c74da55e3970180d70a9888770909728761ae
SirunoDris <j.sirunodri.elluch@gmail.com>
2023-03-20 01:09:11 +0100

    comment view.

--- resources/views/pios/comment.blade.php
+++ resources/views/pios/comment.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-        <h1>COMMENT PAGE</h1>
+        <h1>COMMENT</h1>
         <div class="mt-4 bg-white shadow-sm rounded-lg divide-y">
             <div class="p-6 flex space-x2">
                 <div class="flex-1">
